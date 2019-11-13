48e397ca0ad14d6106bb3a29920e6f405e07c967
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2019-11-13 20:57:08 +0000

    Update deploy script

--- ha-client/deploy.sh
+++ ha-client/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker build -t ha-client .
-docker kill ha-client
-docker run -d -p 8080:8080 --restart unless-stopped ha-client
+docker kill home-automation
+docker run -d -p 8080:8080 --restart unless-stopped --name home-automation ha-client
 
