3a93137f644028dfe9584ae8a0181e4072331298
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2019-11-18 20:51:45 +0000

    Major refactor of event handling

--- ha-alexa-skills/easybulb_skill.py
+++ ha-alexa-skills/easybulb_skill.py
@@ -3,7 +3,11 @@ import json
 import time
 import logging
 import boto3
+from functools import partial
 from botocore.exceptions import ClientError, ParamValidationError
+
+class InvalidMethodException(Exception):
+    pass
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -83,110 +87,86 @@ def construct_success_response(context_result, response_header, request_token):
         }
     }
 
-
-def handle_power_control(event, context):
+def event_handler_wrapper(handler, event, context):
+    request_namespace = event['directive']['header']['namespace']
     request_method = event['directive']['header']['name']
     request_token = event['directive']['endpoint']['scope']['token']
+    request_payload = event['directive'].get('payload', None)
 
     response_header = construct_response_header(event)
 
-    if request_method == "TurnOn":
-        power_result = "ON"
-    elif request_method == "TurnOff":
-        power_result = "OFF"
-    else:
+    try:
+        result = handler(request_method, request_payload)
+    except (ClientError, ParamValidationError) as e:
+        logging.error(e)
+        return construct_sqs_error_response(response_header)
+    except InvalidMethodException as e:
+        logging.error(e)
         return construct_method_error_response(response_header, request_method)
 
-    sqs_message = json.dumps({'service': 'easybulb', 'action': request_method})
-    sqs_result = send_sqs_message(sqs_message)
-
-    if sqs_result is None:
-        return construct_sqs_error_response(response_header)
-
     context_result = {
-        "properties": [{
-            "namespace": "Alexa.PowerController",
-            "name": "powerState",
-            "value": power_result,
-            "timeOfSample": time.strftime("%Y-%m-%dT%H:%M:%S.00Z", time.gmtime()),
-            "uncertaintyInMilliseconds": 500
-        }]
-    }
-
+            "properties": [{
+                "namespace": request_namespace,
+                "name": result['name'],
+                "value": result['value'],
+                "timeOfSample": time.strftime("%Y-%m-%dT%H:%M:%S.00Z", time.gmtime()),
+                "uncertaintyInMilliseconds": 500
+            }]
+        }
+    
     return construct_success_response(context_result, response_header, request_token)
 
 
-def handle_colour_control(event, context):
-    request_method = event['directive']['header']['name']
-    request_token = event['directive']['endpoint']['scope']['token']
+def power_handler(method, payload):
+    if method == "TurnOn":
+        power_result = "ON"
+    elif method == "TurnOff":
+        power_result = "OFF"
+    else:
+        raise InvalidMethodException("Invalid PowerControl method: " + method)
 
-    response_header = construct_response_header(event)
+    sqs_message = json.dumps({'service': 'easybulb', 'action': method})
+    send_sqs_message(sqs_message)
 
-    if request_method != 'SetColor':
-        return construct_method_error_response(response_header, request_method)
+    return { "name": "powerState", "value": power_result }
 
-    colour = event['directive']['payload']['color']
+
+def colour_handler(method, payload):
+    if method != 'SetColor':
+        raise InvalidMethodException("Invalid PowerControl exception: " + method)
+
+    colour = payload['color']
     hsv = [str(colour[k]) for k in ['hue', 'saturation', 'brightness']]
     colour_str = ", ".join(hsv)
 
     sqs_message = json.dumps({
         "service": "easybulb",
-        "action": request_method,
+        "action": method,
         "value": colour_str
     })
     sqs_result = send_sqs_message(sqs_message)
 
-    if sqs_result is None:
-        return construct_sqs_error_response(response_header)
+    return { "name": "color", "value": colour }
 
-    context_result = {
-        "properties": [{
-            "namespace": "Alexa.ColorController",
-            "name": "color",
-            "value": colour,
-            "timeOfSample": time.strftime("%Y-%m-%dT%H:%M:%S.00Z", time.gmtime()),
-            "uncertaintyInMilliseconds": 500
-        }]
-    }
 
-    return construct_success_response(context_result, response_header, request_token)
+def brightness_handler(method, payload):
+    if method != 'SetBrightness':
+        raise InvalidMethodException("Invalid BrightnessControl method: " + method)
 
-def handle_brightness_control(event, context):
-    request_method = event['directive']['header']['name']
-    request_token = event['directive']['endpoint']['scope']['token']
-
-    response_header = construct_response_header(event)
-
-    if request_method != 'SetBrightness':
-        return construct_method_error_response(response_header, request_method)
-
-    brightness = event['directive']['payload']['brightness']
+    brightness = payload['brightness']
     brightness_str = str(brightness)
 
     sqs_message = json.dumps({
         "service": "easybulb",
-        "action": request_method,
+        "action": method,
         "value": brightness
     })
     sqs_result = send_sqs_message(sqs_message)
 
-    if sqs_result is None:
-        return construct_sqs_error_response(response_header)
-
-    context_result = {
-        "properties": [{
-            "namespace": "Alexa.ColorController",
-            "name": "brightness",
-            "value": brightness,
-            "timeOfSample": time.strftime("%Y-%m-%dT%H:%M:%S.00Z", time.gmtime()),
-            "uncertaintyInMilliseconds": 500
-        }]
-    }
-
-    return construct_success_response(context_result, response_header, request_token)
+    return { "name": "brightness", "value": brightness }
 
 
-def handle_discovery(event, context):
+def discovery_handler(event, context):
     payload = {
         'endpoints': [
             {
@@ -241,17 +221,17 @@ def handle_discovery(event, context):
 
 
 def lambda_handler(event, context):
-    if event['directive']['header']['namespace'] == 'Alexa.Discovery' and event['directive']['header']['name'] == 'Discover':
-        logger.debug('Discover request: {}'.format(json.dumps(event)))
-        return handle_discovery(event, context)
-    elif event['directive']['header']['namespace'] == 'Alexa.PowerController':
-        logger.debug('PowerController request: {}'.format(json.dumps(event)))
-        return handle_power_control(event, context)
-    elif event['directive']['header']['namespace'] == 'Alexa.ColorController':
-        logger.debug('ColorController request: {}'.format(json.dumps(event)))
-        return handle_colour_control(event, context)
-    elif event['directive']['header']['namespace'] == 'Alexa.BrightnessController':
-        logger.debug('BrightnessController request: {}'.format(json.dumps(event)))
-        return handle_brightness_control(event, context)
+    event_handlers = {
+        "Alexa.Discovery": discovery_handler,
+        "Alexa.PowerController": partial(event_handler_wrapper, power_handler),
+        "Alexa.ColorController": partial(event_handler_wrapper, colour_handler),
+        "Alexa.BrightnessController": partial(event_handler_wrapper, brightness_handler)
+    }
+    supported_namespaces = event_handlers.keys()
+
+    namespace = event['directive']['header']['namespace']
+    if namespace in supported_namespaces:
+        logger.debug('{} request: {}'.format(namespace, json.dumps(event)))
+        return event_handlers[namespace](event, context)
 
     logger.error('Unknown request received: {}'.format(json.dumps(event)))
