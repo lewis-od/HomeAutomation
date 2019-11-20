abdf78644f2a5c578bd12392d86861b1f6407254
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2019-11-20 21:43:44 +0000

    Make logs available on host

--- ha-client/deploy.sh
+++ ha-client/deploy.sh
@@ -3,5 +3,4 @@
 docker build -t ha-client .
 docker kill home-automation
 docker rm home-automation
-docker run -d -p 8080:8080 --restart unless-stopped --name home-automation ha-client
-
+docker run -d -p 8080:8080 -v "$(pwd)"/logs:/app/logs --restart unless-stopped --name home-automation ha-client
