bc5fc5d21538a9363c66c97e37b1eea4fba1152c
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2019-11-24 19:12:38 +0000

    Add more LocalHaController tests

--- ha-client/src/test/java/uk/co/lewisodriscoll/haclient/controller/LocalHaControllerTest.java
+++ ha-client/src/test/java/uk/co/lewisodriscoll/haclient/controller/LocalHaControllerTest.java
@@ -1,23 +1,21 @@
 package uk.co.lewisodriscoll.haclient.controller;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.co.lewisodriscoll.haclient.HaClientApplication;
+import uk.co.lewisodriscoll.haclient.domain.HaColour;
+import uk.co.lewisodriscoll.haclient.helper.BrightnessHelper;
 import uk.co.lewisodriscoll.haclient.model.HaAction;
 import uk.co.lewisodriscoll.haclient.model.HaResponse;
 import uk.co.lewisodriscoll.haclient.service.ActionIngestionService;
+
+import java.awt.Color;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -35,6 +33,33 @@ public class LocalHaControllerTest {
                 .action("TurnOn")
                 .build();
     }
+
+    private HaAction givenOffAction() {
+        return HaAction.builder()
+                .service("easybulb")
+                .action("TurnOff")
+                .build();
+    }
+
+    private Color givenColour = new Color(255, 164, 48);
+    private HaAction givenSetColourAction() {
+        HaColour givenHaColour = new HaColour(givenColour);
+        return HaAction.builder()
+                .service("easybulb")
+                .action("SetColor")
+                .value(givenHaColour.toString())
+                .build();
+    }
+
+    private int givenBrightness = 75;
+    private HaAction givenSetBrightnesAction() {
+        return HaAction.builder()
+                .service("easybulb")
+                .action("SetBrightness")
+                .value(String.valueOf(givenBrightness))
+                .build();
+    }
+
 
     private HaResponse successResponse() {
         return HaResponse.builder()
@@ -54,6 +79,41 @@ public class LocalHaControllerTest {
         given(ingestionService.ingest(givenOnAction())).willReturn(successResponse());
 
         mockMvc.perform(get("/api/on")
+                .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("message").isString());
+    }
+
+    @Test
+    public void testGetOff() throws Exception {
+        given(ingestionService.ingest(givenOffAction())).willReturn(successResponse());
+
+        mockMvc.perform(get("/api/off")
+                .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("message").isString());
+    }
+
+    @Test
+    public void testGetColour() throws Exception {
+        given(ingestionService.ingest(givenSetColourAction())).willReturn(successResponse());
+
+        String endpoint = String.format("/api/colour?r=%s&g=%s&b=%s",
+                givenColour.getRed(), givenColour.getGreen(), givenColour.getBlue());
+
+        mockMvc.perform(get(endpoint)
+                .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("message").isString());
+    }
+
+    @Test
+    public void testGetBrightness() throws Exception {
+        given(ingestionService.ingest(givenSetBrightnesAction())).willReturn(successResponse());
+
+        String endpoint = String.format("/api/brightness?percentage=%d", givenBrightness);
+
+        mockMvc.perform(get(endpoint)
                 .contentType("application/json"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("message").isString());
